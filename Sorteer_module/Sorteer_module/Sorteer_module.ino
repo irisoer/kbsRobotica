7417fe3e75e4e2481e902369605b31e7b56cf8f0
krijn <grimmekrijn@gmail.com>
2022-05-24 13:18:20 +0200

    Sorteer module code

---
+++ Sorteer_module/Sorteer_module/Sorteer_module.ino
@@ -0,0 +1,214 @@
+
+#include <Servo.h>
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+#define STEPPER_PIN_1 9
+#define STEPPER_PIN_2 10
+#define STEPPER_PIN_3 11
+#define STEPPER_PIN_4 12
+int step_number = 0;
+Servo Myservo;
+String inputservo;
+String laatst;
+bool onder = false;
+int returnInt = 0;
+#define draai 0
+#define stil 1
+#define allesstil 2
+bool codenetgestart = true;
+
+
+
+/* kleurensensor:
+   SCL -> A5        blauw
+   SDA -> A4        geel
+   VDD -> 3.3V      groen 
+   ground -> GROUND wit
+
+
+   servo -> 3
+
+   steppenmotor:
+   PIN_1 -> 9
+   PIN_2 -> 10
+   PIN_3 -> 11
+   PIN_4 -> 12
+
+
+
+*/
+
+/* Initialise with default values (int time = 2.4ms, gain = 1x) */
+Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+
+/* Initialise with specific int time and gain values */
+//Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+
+
+
+void setup(void) {
+
+  Serial.setTimeout(10);
+
+  pinMode(STEPPER_PIN_1, OUTPUT);
+  pinMode(STEPPER_PIN_2, OUTPUT);
+  pinMode(STEPPER_PIN_3, OUTPUT);
+  pinMode(STEPPER_PIN_4, OUTPUT);
+
+  Serial.begin(9600);
+
+  Myservo.attach(3);
+
+  while (!tcs.begin()) {
+  } 
+
+  Myservo.write(0);
+
+  Serial.println("!s");
+  waitForSerialChar(':');
+}
+
+
+
+
+void loop() {
+  switch (returnInt) {
+    case draai:
+
+      OneStep(true);
+      delay(3);
+
+
+      uint16_t r, g, b, c, colorTemp, lux;
+
+      tcs.getRawData(&r, &g, &b, &c);
+
+      if (c >= 6 && !onder && !codenetgestart) {
+        if (r > b && r > g && r > 4 && laatst != "r") {
+          laatst = "r";
+          onder = true;
+
+          // Serial.print("Rood: "); Serial.print(r, DEC); Serial.print(" ");
+        } else if (b > r && b > g && b > 8 && laatst != "b") {
+          laatst = "b";
+          onder = true;
+
+          //Serial.print("Blauw: "); Serial.print(b, DEC); Serial.print(" ");
+        } else if (g > r && g > b && r > 3 && laatst != "g") {
+          laatst = "g";
+          onder = true;
+
+          //Serial.print("Groen: "); Serial.print(g, DEC); Serial.print(" ");
+        }//
+      } else if (onder && !codenetgestart) {
+
+        Serial.println("#" + laatst);
+        onder = false;
+        returnInt = 2;
+        //    laatst = "";
+      } else if (c < 6 && codenetgestart) {
+        codenetgestart = false;
+      } else {
+        onder = false;
+        laatst = "";
+      }
+
+      break;
+
+    case stil:
+
+      Myservo.write(180);
+      delay(1000);
+      Myservo.write(0);
+      delay(1000);
+
+      returnInt = 0;
+      break;
+
+
+    case allesstil:
+
+      waitForSerialChar(':');
+
+
+
+
+
+  }
+}
+
+
+void waitForSerialChar(char c) {
+  String rString;
+  rString = Serial.readString();
+  rString.trim();
+  while (rString.indexOf(c) == -1) {
+    rString = Serial.readString();
+  }
+  Serial.println(rString[rString.indexOf(c) - 1]);
+  returnInt = int(rString[rString.indexOf(c) - 1]) - '0';
+}
+
+
+void OneStep(bool dir) {
+  if (dir) {
+    switch (step_number) {
+      case 0:
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 1:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, HIGH);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 2:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, HIGH);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 3:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, HIGH);
+        break;
+    }
+  } else {
+    switch (step_number) {
+      case 0:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, HIGH);
+        break;
+      case 1:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, HIGH);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 2:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, HIGH);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 3:
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+
+
+    }
+  }
+  step_number++;
+  if (step_number > 3) {
+    step_number = 0;
+  }
+}
