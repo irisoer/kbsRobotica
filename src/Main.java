a88bdbd7c333fa9436cfd7c2a7a6fbd8858cd436
iris <s177355@student.windesheim.nl>
2022-05-24 13:31:58 +0200

    Order Class met "printen" van de pakbon

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import jssc.SerialPortException;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -50,28 +51,64 @@ public class Main {
 //        System.out.println(bpp);
 //    }
 
-	public static void main(String[] args) throws SerialPortException, SQLException, InterruptedException {
-        ArduinoInpak inpak = new ArduinoInpak();
-//        inpak.draaiNaarPlatform(0);
+    //	public static void main(String[] args) throws SerialPortException, SQLException, InterruptedException {
+//        ArduinoInpak inpak = new ArduinoInpak();
+////        inpak.draaiNaarPlatform(0);
+//
+//		ArduinoSorteer sorteer = new ArduinoSorteer();
+//        ArrayList<Artikel> result = new ArrayList<>();
+//        for (int i = 0; i < 3; i++) {
+//            result.add(Database.selecteerArtikel(60));
+//            result.add(Database.selecteerArtikel(73));
+//        }
+//        result.add(Database.selecteerArtikel(70));
+//        Bpp bpp = new Bpp(result, 12);
+//
+//        System.out.println(bpp);
+//        while(!bpp.isLeeg()) {
+//            int index = sorteer.getKleur(bpp);
+//            System.out.println(index);
+//            inpak.draaiNaarPlatform(index);
+//            System.out.println(bpp);
+//
+//        }
+//	}
+//    public static void main(String[] args) throws SQLException {
+//        System.out.println("-------------------------------");
+//        Database voorraad = new Database();
+//        ArrayList<Integer> getvoorraadArray = new ArrayList<>();
+//        getvoorraadArray.add(Database.getVoorraad(60));
+//        getvoorraadArray.add(Database.getVoorraad(70));
+//        getvoorraadArray.add(Database.getVoorraad(73));
+//        for (Integer voorraad1 : getvoorraadArray) {
+//            System.out.println(voorraad1);
+//        }
+////    result.add(Database.selecteerArtikel(70));
+////    Bpp bpp = new Bpp(result, 12);
+////    System.out.println(bpp);
+////    while(!bpp.isLeeg()) {
+////        int index = sorteer.getKleur(bpp);
+////        System.out.println(index);
+//////            inpak.draaiNaarPlatform(index);
+////        System.out.println(bpp);
+//
+//        ArrayList<Integer> updatedVoorraadArray = new ArrayList<>();
+//        updatedVoorraadArray.add(Database.updateVoorraad(60, 4));
+//        updatedVoorraadArray.add(Database.updateVoorraad(70, 7));
+//        updatedVoorraadArray.add(Database.updateVoorraad(73, 7));
+//        for (Integer voorraad2 : updatedVoorraadArray) {
+//            System.out.println(voorraad2);
+//        }
 
-		ArduinoSorteer sorteer = new ArduinoSorteer();
-        ArrayList<Artikel> result = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            result.add(Database.selecteerArtikel(60));
-            result.add(Database.selecteerArtikel(73));
-        }
-        result.add(Database.selecteerArtikel(70));
-        Bpp bpp = new Bpp(result, 12);
 
-        System.out.println(bpp);
-        while(!bpp.isLeeg()) {
-            int index = sorteer.getKleur(bpp);
-            System.out.println(index);
-            inpak.draaiNaarPlatform(index);
-            System.out.println(bpp);
-
-        }
-	}
-
+public static void main(String[] args) throws IOException, SQLException {
+    Order order = new Order();
+    order.maakPakbon(20);
 
 }
+
+
+
+
+    }
+
---
+++ src/Order.java
@@ -0,0 +1,71 @@
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Order extends Database {
+    private Database database;
+    private Bpp bpp;
+    private ArrayList<Artikel> artikelen;
+    private int klantID = 0;
+    private String klantNaam = null;
+    private String klantAdres = null;
+    private String klantPostcode = null;
+    private String stadNaam = null;
+
+    public Order() {
+    }
+
+    public void getOrder(){
+
+    }
+
+    public void maakPakbon(int customerId) throws IOException, SQLException {
+        startConnection();
+        PreparedStatement preparedStatement = getConnection().prepareStatement("SELECT CustomerID, CustomerName, DeliveryAddressLine1, DeliveryPostalCode, CityName FROM nerdygadgets.customers AS cu \n" +
+                "LEFT JOIN nerdygadgets.cities AS ci ON cu.PostalCityID = ci.CityID \n" +
+                "WHERE CustomerID = ?");
+        preparedStatement.setInt(1, customerId);
+        ResultSet result = preparedStatement.executeQuery();
+        while (result.next()) {
+            klantID = result.getInt("CustomerID");
+            klantNaam = result.getString("CustomerName");
+            klantAdres = result.getString("DeliveryAddressLine1");
+            klantPostcode = result.getString("DeliveryPostalCode");
+            stadNaam = result.getString("CityName");
+        }
+        result.close();
+
+        XWPFDocument document = new XWPFDocument();
+
+        XWPFParagraph p1 = document.createParagraph();
+        XWPFRun run1 = p1.createRun();
+        run1.setBold(false);
+        run1.setText("KlantNr: " + klantID);
+        run1.addBreak();
+        run1.setText(klantNaam);
+        run1.addBreak();
+        run1.setText(klantAdres);
+        run1.addBreak();
+        run1.setText(klantPostcode + " " + stadNaam);
+        run1.addBreak();
+        run1.addBreak();
+
+        XWPFParagraph p2 = document.createParagraph();
+        XWPFRun run2 = p2.createRun();
+        run2.setBold(true);
+        run2.setText("Uw bestelling: ");
+
+
+        document.write(new FileOutputStream("Order.docx"));
+    }
+}
+
+
