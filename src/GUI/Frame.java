56d9048bcf28dacbe8049afbf144dc8a4e42e12c
krijn <grimmekrijn@gmail.com>
2022-05-28 18:55:58 +0200

    Werkend frame met cardlayout

--- src/GUI/Frame.java
+++ src/GUI/Frame.java
@@ -3,46 +3,57 @@ package GUI;
 import java.awt.*;
 import javax.swing.*;
 
-public class Frame extends JFrame implements Layout{
+public class Frame extends JFrame implements Layout {
         JPanel JPschermen;
-        public StartScherm startScherm = new StartScherm();
         CardLayout cards;
         Schermen huidigScherm;
 
 
         public enum Schermen {
-                START,
-                CAROUSSEL,
-                EIND
+                StartScherm(new StartScherm()),
+                VerwerkScherm(new VerwerkScherm());
+                private GUI.Scherm scherm;
+                Schermen(GUI.Scherm scherm) {
+                        this.scherm = scherm;
+                }
+
+                @Override
+                public String toString() {
+                        return this.scherm.getName();
+                }
         }
         public Frame(){
                 super("GUI");
                 cards = new CardLayout();
                 JPschermen = new JPanel(cards);
                 setSize(800, 480);
-                setLayout(cards);
+                setResizable(false);
+
+                setLayout(null);
                 setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-
                 setPreferredSize(new Dimension(800, 480));
                 setMaximumSize(new Dimension(800, 480));
                 setMinimumSize(new Dimension(800, 480));
                 setLayout(new CardLayout());
                 setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-                JPschermen.add(startScherm, String.valueOf(Schermen.START));
-                JPschermen.add(new VerwerkScherm(), String.valueOf(Schermen.CAROUSSEL));
-                JPschermen.add(new Panel(), String.valueOf(Schermen.EIND));
+                for (Schermen scherm: Schermen.values()) {
+                        addSchermToCards(scherm, JPschermen);
+                }
 
 
-                add(startScherm);
-                setScherm(Schermen.START);
+                add(JPschermen);
+                setScherm(Schermen.StartScherm);
                 setResizable(false);
                 setVisible(true);
         }
 
         public void setScherm(Schermen scherm) {
-                cards.show(JPschermen, String.valueOf(scherm));
+                cards.show(JPschermen, scherm.toString());
                 huidigScherm = scherm;
+        }
+
+        private void addSchermToCards(Schermen scherm, JPanel cardPanel) {
+                cardPanel.add(scherm.scherm, scherm.scherm.getName());
         }
 }
